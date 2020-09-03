8ce4d0f185fc1def349acb67633bf005d9276c6c
smtaha512 <s.m.taha10@gmail.com>
2020-09-03 17:45:41 +0500

    build: configure graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { environment } from './environments/environment';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({ ...environment.database })],
+  imports: [
+    TypeOrmModule.forRoot({ ...environment.database }),
+    GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+      autoSchemaFile: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
