3513bd3c50281d788502989815390c7d70883848
smtaha512 <s.m.taha10@gmail.com>
2020-09-08 14:58:30 +0500

    feat: allow requested fields decorator to have raw and strings mode

--- src/shared/decorators/gql-requested-fields.ts
+++ src/shared/decorators/gql-requested-fields.ts
@@ -3,12 +3,29 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 import { GraphQLResolveInfo } from 'graphql';
 import * as graphqlFields from 'graphql-fields';
 
-export const RequestedFields = createParamDecorator(
-  (_, ctx: ExecutionContext) => {
-    return Object.keys(
-      graphqlFields(
-        GqlExecutionContext.create(ctx).getInfo<GraphQLResolveInfo>(),
-      ),
+export enum RequestedFieldsMode {
+  RAW,
+  STRINGS,
+}
+
+/**
+ * @description Extract and parse requested fields by graphql clients into string or returns raw extracted object depending upon the mode selected
+ * Default mode is `RequestedFieldsMode#STRINGS`
+ */
+
+export const RequestedFields: (
+  mode?: RequestedFieldsMode,
+) => ParameterDecorator = createParamDecorator(
+  (
+    mode: RequestedFieldsMode = RequestedFieldsMode.STRINGS,
+    ctx: ExecutionContext,
+  ) => {
+    const rawFields = graphqlFields(
+      GqlExecutionContext.create(ctx).getInfo<GraphQLResolveInfo>(),
     );
+    if (mode === RequestedFieldsMode.RAW) {
+      return rawFields;
+    }
+    return Object.keys(rawFields);
   },
 );
